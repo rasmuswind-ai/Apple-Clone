0dcd3bf1262972946a9dce89a88adc864173f903
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-10 20:47:34 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Hero.jsx 	modified:   src/components/Highlights.jsx 	new file:   src/components/VideoCarousel.jsx

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react'
+import { hightlightsSlides } from '../constants'
+
+const VideoCarousel = () => {
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+
+    const [video, setvideo] = useState({
+        isEnd: false,
+        startPlay: false,
+        videoId: 0,
+        isLastVideo: false,
+        isPlaying: false,
+    })
+
+    const { isEnd, isLastVideo, startPlay, videoId, isPlaying} = video;
+
+    useEffect(() => {
+        const currentProgress = 0;
+        let span = videoSpanRef.current;
+
+        if(span[videoId]) {
+            // animate the progress of the video
+            
+        }
+    }, [videoId, startPlay])
+
+    return (
+    <>
+        <div className='flex items-center'>
+            {hightlightsSlides.map((list, i) => (
+                <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
+                    <div className="video-carousel_container">
+                        <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                            <video id='video' playsInline={true} preload='auto' muted>
+                                <source src={list.video} type='video/mp4'/>
+                            </video>
+                        </div>
+
+                        <div className='absolute top-12 left-[5%] z-10'>
+                            {list.textLists.map((text) => (
+                                <p key={text} className='md:text-2xl text-xl font-medium'>
+                                    {text}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </>
+  )
+}
+
+export default VideoCarousel
