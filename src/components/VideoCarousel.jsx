11bf486bb7db70d1019b8d4210afed2c9dd94d95
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-10 20:55:29 +0200

    modified:   src/components/VideoCarousel.jsx

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from 'react'
 import { hightlightsSlides } from '../constants'
+import gsap from 'gsap';
 
 const VideoCarousel = () => {
     const videoRef = useRef([]);
     const videoSpanRef = useRef([]);
     const videoDivRef = useRef([]);
 
-    const [video, setvideo] = useState({
+    const [Video, setVideo] = useState({
         isEnd: false,
         startPlay: false,
         videoId: 0,
@@ -14,7 +15,19 @@ const VideoCarousel = () => {
         isPlaying: false,
     })
 
-    const { isEnd, isLastVideo, startPlay, videoId, isPlaying} = video;
+    const [loadedData, setloadedData] = useState([])
+
+    const { isEnd, isLastVideo, startPlay, videoId, isPlaying} = Video;
+
+    useEffect(() => {
+        if(loadedData.length > 3) {
+            if(!isPlaying) {
+                videoRef.current[videoId].pause();
+            } else {
+                startPlay && videoRef.current[videoId].play();
+            }
+        }
+    }, [startPlay, videoId, isPlaying, loadedData])
 
     useEffect(() => {
         const currentProgress = 0;
@@ -22,7 +35,15 @@ const VideoCarousel = () => {
 
         if(span[videoId]) {
             // animate the progress of the video
-            
+            let anim = gsap.to(span[videoId], {
+                onUpdate: () => {
+
+                },
+
+                onComplete: () => {
+
+                }
+            })
         }
     }, [videoId, startPlay])
 
@@ -33,7 +54,17 @@ const VideoCarousel = () => {
                 <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
                     <div className="video-carousel_container">
                         <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                            <video id='video' playsInline={true} preload='auto' muted>
+                            <video 
+                                id='video' 
+                                playsInline={true} 
+                                preload='auto' 
+                                muted 
+                                ref={(el) => (videoRef.current[i] = el)} 
+                                onPlay={() => {
+                                    setVideo((prevVideo) => ({
+                                        ...prevVideo, isPlaying: true
+                                    }))
+                                }}>
                                 <source src={list.video} type='video/mp4'/>
                             </video>
                         </div>
