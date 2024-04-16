8cf18209da030b012005ab231ba205a8a8770169
Rasmus Wind <rasmuswind7906@gmail.com>
2024-04-16 11:05:49 +0200

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,6 @@ function Navbar() {
             </div>
 
             <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-                <img src={searchImg} alt="search" width={18} height={18} />
                 <img src={bagImg} alt="bag" width={18} height={18} />
             </div>
         </nav>
